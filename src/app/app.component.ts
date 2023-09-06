28354a4874eb401344fa4a18855a5b5f5c02f47f
Vito <lolozol80xd@gmail.com>
2023-09-06 08:30:07 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projekt2';
+  obj =[
+    {name:"Béla",age:31,hair:"barna"},
+    {name:"Attila",age:48,hair:"őszül"},
+    {name:"Károly",age:40,hair:"nincs"},
+    {name:"Dezső",age:55,hair:"fekete"},
+    {name:"Józsi",age:55,hair:"fekete"},
+    {name:"Sallai",age:56,hair:"barna"},
+  ]
 }
